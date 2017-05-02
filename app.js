1aa62af68c659cdc5707c54640203ac465ea4422
Joe Spedale <jspedale@gmail.com>
2017-05-02 18:48:07 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,45 @@
+let ctx = document.getElementById('chart').getContext('2d');
+
+const cityJobsData = fetch('https://data.cityofnewyork.us/resource/swhp-yxa4.json');
+
+cityJobsData
+  .then(data => data.json())
+  .then(data=> {
+    const agencyFrequency = data.reduce((agencies, value) => {
+      agencies[value.agency] = agencies.hasOwnProperty(value.agency) ? agencies[value.agency] + 1 : 1;
+      return agencies;
+    }, {})
+    console.log(agencyFrequency);
+    const agencyKeys = Object.keys(agencyFrequency);
+    console.log(agencyKeys);
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: agencyKeys,
+        datasets: [{
+          label: 'jobs',
+          data: Object.values(agencyFrequency),
+          backgroundColor: "rgba(153, 255, 51, 0.4)"
+        }]
+      }
+    })
+  })
+  .then(agencies => {
+    console.log(agencies);
+    // const agencyKeys = Object.keys(agencies);
+    // console.log(agencyKeys);
+    // const myChart = new Chart(ctx, {
+    //   type: 'bar',
+    //   data: {
+    //     labels:
+    //   }
+    // })
+  })
+
+
+// const myChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//
+//   }
+// })
